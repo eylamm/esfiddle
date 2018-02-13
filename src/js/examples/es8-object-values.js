3cb629241e6dd6f46023cebf39b571872054ef34
Josh Ghent <me@joshghent.com>
2018-02-13 12:06:48 +0000

    Added object values example.

---
+++ src/js/examples/es8-object-values.js
@@ -0,0 +1,9 @@
+/* global window */
+window.es8Example = window.es8Example || {};
+window.es8Example.objectvalues = {};
+
+window.es8Example.objectvalues.code =
+`console.log(Object.values({1: 'wasd', 2: 356}));
+`;
+
+window.es8Example.objectvalues.display = 'Object Values';
